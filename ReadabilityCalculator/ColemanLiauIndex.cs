4c3d15b263d0e8e94dcf3f193405c49b406986f4
Lance <lancedbeachy@gmail.com>
2020-05-06 17:44:30 -0400

    Added Classes for 4 Formulas
    Added 4 different classes to take care of the 4 different formulas I planned on using.

---
+++ ReadabilityCalculator/ColemanLiauIndex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace ReadabilityCalculator
+{
+    public class ColemanLiauIndex : IReadingScore
+    {
+        public string InputText { get; }
+        public double NumberOfWords { get; }
+        public double NumberOfSentences { get; }
+        public double NumberOfSyllables { get; }
+        public double NumberOfCharacters { get; }
+        public double ReadingScore { get; }
+        public MatchCollection Matches { get; set; }
+
+        public ColemanLiauIndex(string inputText)
+        {
+            InputTextParser parser = new InputTextParser(inputText);
+            InputText = inputText;
+            NumberOfWords = parser.GetNumberOfWords();
+            NumberOfSentences = parser.GetNumberOfSentences();
+            NumberOfSyllables = parser.GetNumberOfSyllables();
+            NumberOfCharacters = parser.GetNumberOfCharacters();
+            ReadingScore = CalculateScore();
+        }
+
+        double CalculateScore()
+        {
+            double score = .0588 * (NumberOfCharacters / (NumberOfCharacters/100)) - .296 * (NumberOfSentences / (NumberOfWords / 100)) - 15.8;
+            return Math.Round(score, 2);
+        }
+    }
+}
---
+++ ReadabilityCalculator/FleschReadingEase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using ReadabilityCalculator.Models;
+
+namespace ReadabilityCalculator
+{
+    public class FleschReadingEase : IReadingScore
+    {
+        public string InputText { get; }
+        public double NumberOfWords { get; }
+        public double NumberOfSentences { get; }
+        public double NumberOfSyllables { get; }
+        public double NumberOfCharacters { get; }
+        public double ReadingScore { get; }
+
+        public FleschReadingEase(string inputText)
+        {
+            InputTextParser parser = new InputTextParser(inputText);
+            InputText = inputText;
+            NumberOfWords = parser.GetNumberOfWords();
+            NumberOfSentences = parser.GetNumberOfSentences();
+            NumberOfSyllables = parser.GetNumberOfSyllables();
+            NumberOfCharacters = parser.GetNumberOfCharacters();
+            ReadingScore = CalculateScore();
+        }
+
+        
+        public double CalculateScore()
+        {
+            double score = 206.835 - (1.015 * (NumberOfWords / NumberOfSentences)) - 84.6 * (NumberOfSyllables / NumberOfWords);
+            if (score > 100)
+            {
+                score = 100;
+            }
+            return Math.Round(score, 2);
+        }
+    }
+}
